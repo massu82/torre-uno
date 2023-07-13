47304c43ca8863bce1456560ad99942090919d19
Massuttier Studio <72637167+massu82@users.noreply.github.com>
2023-07-13 13:19:29 -0600

    plantas

--- include/avance-video.php
+++ include/avance-video.php
@@ -9,9 +9,14 @@
         <div class="row">
             <div class="col-12 col-lg-12 fit-videos text-center md-margin-30px-bottom">
                 <!-- start vimeo video -->
-                <iframe width="100%" height="615"
+                <!--<iframe width="100%" height="615"
                         src="https://www.youtube.com/embed/Dz9-be1Z-I8"
-                        frameborder="0"></iframe>
+                        frameborder="0"></iframe>-->
+                <iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2FBanyanTreeCM.Residences%2Fvideos%2F1258390854791140%2F&show_text=false&width=267&t=0"
+                        width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0"
+                        allowfullscreen="true"
+                        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+                        allowFullScreen="true"></iframe>
                 <!-- end vimeo video -->
             </div>
         </div>
